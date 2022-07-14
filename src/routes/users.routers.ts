664e2e79b0c7be57ebccd86d54f3c08948cd8cf4
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-14 19:54:20 -0300

    add register user controller to user post route

--- src/routes/users.routers.ts
+++ src/routes/users.routers.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-
+import { RegisterUserController } from "../controller/RegisterUser.controller";
+import { UserRepository } from "../repository/user.repository";
 const UserRouter = Router();
-
-UserRouter.post("/", async (req, res) => {
-  res.send({ oi: "oi" });
-});
+const userRepository = new UserRepository();
+const registerUser = new RegisterUserController(userRepository);
+UserRouter.post("/", registerUser.handle);
 
 UserRouter.get("/", async (req, res) => {
   res.send("Hello world");
