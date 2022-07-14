205034170f96030209a4609cc54fca3771eab538
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-14 19:14:09 -0300

    use UserRouter by express

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import UserRouter from "./routes/users.routers";
 
 const app = express();
 
 app.use(express.json());
+app.use("/user", UserRouter);
 
 export default app;
