711011362b8094ed244197099dbc3a78dad40a76
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-14 19:33:22 -0300

    add mongoose

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,19 @@ import UserRouter from "./routes/users.routers";
 
 const app = express();
 
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    `mongodb+srv://pedrobuenoxs:12345@dev-challenge.wwuxf6z.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch((err) => {
+    console.log("Error while connecting database::", err);
+  });
+
 app.use(express.json());
 app.use("/user", UserRouter);
 
