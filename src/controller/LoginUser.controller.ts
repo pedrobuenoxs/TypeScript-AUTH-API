f31f416af07038e6da3ee7cea7993962d0aed922
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-15 12:48:19 -0300

    response refact

--- src/controller/LoginUser.controller.ts
+++ src/controller/LoginUser.controller.ts
@@ -8,7 +8,7 @@ export class LoginController {
     const { name, password } = req.body;
     try {
       const token = await this.userService.handle(name, password);
-      res.json({ token: token }).status(200);
+      res.json({ token }).status(200);
     } catch (error) {
       return res.json({ error: error.message }).status(400);
     }
