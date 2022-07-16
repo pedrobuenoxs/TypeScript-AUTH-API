7622b8fe55c58c4391b5799240d9862d9354190a
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-16 19:55:37 -0300

    controllers reoganized

--- src/composer/User.compose.ts
+++ src/composer/User.compose.ts
@@ -1,13 +1,16 @@
-import { RegisterUserController } from "../controller/RegisterUser.controller";
+import { RegisterUserController } from "../controller/Public/RegisterUser.controller";
 import { UserRepository } from "../repository/user.repository";
-import { LoginController } from "../controller/LoginUser.controller";
+import { LoginController } from "../controller/Public/LoginUser.controller";
 import { LoginUserService } from "../services/LoginUser.service";
 import { RegisterUserService } from "../services/RegisterUser.service";
 const userRepository = new UserRepository();
 
+//POST '/USER/LOGIN' - To login and receive a token
 const loginUserService = new LoginUserService(userRepository);
-const registerUserService = new RegisterUserService(userRepository);
-
-const registerUser = new RegisterUserController(registerUserService);
 const loginUser = new LoginController(loginUserService);
+
+//POST '/USER' - To register
+const registerUserService = new RegisterUserService(userRepository);
+const registerUser = new RegisterUserController(registerUserService);
+
 export { registerUser, loginUser };
